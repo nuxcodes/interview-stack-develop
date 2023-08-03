3eeae2290b0ae4519aa70ddae2a188f6b4e8086f
nuxcodes <44252050+nuxcodes@users.noreply.github.com>
2023-08-03 14:55:30 -0700

    Update image urls to match description

--- db/data.sql
+++ db/data.sql
@@ -92,7 +92,7 @@ CREATE TABLE `Product` (
 
 LOCK TABLES `Product` WRITE;
 /*!40000 ALTER TABLE `Product` DISABLE KEYS */;
-INSERT INTO `Product` VALUES (1,'Hat','https://unsplash.com/photos/FZWivbri0Xk','Active'),(2,'Shoes','https://unsplash.com/photos/mk7D-4UCfmg','Active'),(3,'Pants','https://unsplash.com/photos/w2JtIQQXoRU','Active'),(4,'Shirt','https://unsplash.com/photos/vn4dC0yFtg8','InActive'),(5,'Coat','https://unsplash.com/photos/esbfaHABh7Y','InActive');
+INSERT INTO `Product` VALUES (1,'Hat','https://unsplash.com/photos/zVycYmcblDY','Active'),(2,'Shoes','https://unsplash.com/photos/NOpsC3nWTzY','Active'),(3,'Pants','https://unsplash.com/photos/m1m2EZOZVwA','Active'),(4,'Shirt','https://unsplash.com/photos/RqYTuWkTdEs','InActive'),(5,'Coat','https://unsplash.com/photos/K7L5V45Vn1U','InActive');
 /*!40000 ALTER TABLE `Product` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
